e44670122e9b00e0473b9bb944164858070330cf
Hanumant Ukirde <hanumantukirde419@gmail.com>
2025-02-11 18:22:13 +0530

    Initial commit

---
+++ grocery-booking-api/src/main/java/com/example/grocery_booking_api/GroceryBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.grocery_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryBookingApiApplication.class, args);
+	}
+
+}
